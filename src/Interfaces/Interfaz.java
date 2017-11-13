45cc3f60bdac1c62a8223a533e7edb9789728afe
paolaandyco71 <paolaandyco71@gmail.com>
2017-11-13 19:09:59 -0300

    CONEXION DE JAVA CON BD Y MSQL

---
+++ src/Interfaces/Interfaz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+import javax.swing.*;//Importamos TODO de swing
+import java.awt.*;
+
+/**
+ *
+ * @author LAB03
+ */
+ public class Interfaz extends JFrame
+ {
+ 
+    
+   
+    
+    
+}
--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -4,18 +4,32 @@
  * and open the template in the editor.
  */
 package conexion;
-
+//Import Interfaces.Interfaz;
+//import javax.swing.JFrame;
+import javax.swing.JFrame;
+import Interfaces.Interfaz;
 /**
  *
- * @author LAB03
+ * @author LP 13
  */
 public class Conexion {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     Interfaz w = new  Interfaz(); 
+     w.setVisible(true);
+     w.setBounds(0, 0, 400, 400);
+     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+// TODO code application logic here
     }
     
 }
+Clase 3 bd
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
